95387b5ae358e69952c077f28e86eefeba15479d
josebenitez21 <92813843+josebenitez21@users.noreply.github.com>
2022-02-14 01:20:41 -0500

    remember

---
+++ GF/projectGF/src/projectgf/ProjectGF.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectgf;
+import Views.view;
+/**
+ *
+ * @author USUARIO
+ */
+public class ProjectGF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        view View = new view ();
+        View.setVisible(true);
+        
+    }
+    
+}
